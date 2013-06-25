6ab3050e865cb65e61e29e8165025e2ca6b91ee5
John Delgado <johndelgado@MacBook-Pro-de-John.local>
2013-06-24 22:08:51 -0430

    Documentada Symtable.rb

--- SymTable.rb
+++ SymTable.rb
@@ -1,3 +1,10 @@
+#Gabriela Limonta 10-10385
+#John Delgado 10-10196
+
+#Se definen los posibles errores que se pueden reportar desde la tabla de simbolos, con sus respectivos constructores y sus procedimiento
+#to_s requerido para imprimir el error
+
+#Error que permite ver al usuario que se trato de redefinir un simbolo en la tabla de simbolos cuando esto no es posible
 class RedefinirError < RuntimeError
   def initialize(token)
     @token = token
@@ -8,6 +15,7 @@ class RedefinirError < RuntimeError
   end
 end
 
+#Error que permite notificar al usuario sobre que no se pudo actualizar un simbolo en la tabla de simbolos
 class UpdateError < RuntimeError
   def initialize(token)
     @token = token
@@ -18,6 +26,7 @@ class UpdateError < RuntimeError
   end
 end
 
+#Este error permite al usuario saber que no se pudo eliminar el simbolo de la tabla de simbolos
 class DeleteError < RuntimeError
   def initialize(token)
     @token = token
@@ -29,13 +38,18 @@ class DeleteError < RuntimeError
 end
 
 
+#Se define la tabla de simbolos que se utiliza para el proyecto Rangex
+
 class SymTable
+  #construcor de la tabla de simbolos
   def initialize(padre = nil)
     @padre = padre
     @tabla = {}
     @nombres = []
   end
 
+  #metodo que permite insertar un simbolo a la tabla de simbolos, no se pueden redefinir simbolos al momento de
+  #hacer una insercion por esto se chequea esta caracteristica primero
   def insert(token, tipo, es_mutable = true)
     raise RedefinirError::new token if @tabla.has_key?(token.texto)
     @tabla[token.texto] = { :tipo => tipo, :es_mutable => es_mutable, :token => token }
@@ -43,6 +57,8 @@ class SymTable
     self
   end
 
+  #metodo que permite eliminar un simbolo de la tabla de simbolos, si este simbolo no se encuentra entonces se levanta una
+  #excepcion
   def delete(nombre)
     raise DeleteError::new token unless @tabla.has_key?(nombre)
     @tabla.delete(nombre)
@@ -50,16 +66,21 @@ class SymTable
     self
   end
 
+
+  #metodo que permite actualizar un simbolo ya existente en la tabla de simbolos, si el simbolo no esta en la tabla
+  #se procede a levantar una excepcion
   def update(token, tipo, es_mutable)
     raise UpdateError::new token unless @tabla.has_key?(token.texto)
     @tabla[token.texto] = { :tipo => tipo, :es_mutable => es_mutable, :token => token }
     self
   end
 
-  def isMember?(nombre) #Preguntar si podemos usar el '?' :) o cambiar los nombres de los metodos en general
+  #Este metodo permite saber si en la tabla de simbolos el simbolo es miembro de ella con el nombre que se le pasa
+  def isMember?(nombre)
     @tabla.has_key?(nombre)
   end
 
+  #Metodo permite saber si el simbolo esta presente en la tabla pasandole al mismo el nombre
   def find(nombre)
     if @tabla.has_key?(nombre) then
       @tabla[nombre]
